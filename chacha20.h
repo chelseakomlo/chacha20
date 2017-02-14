a9f5a348726e0a56cc0235a55006efd08bd99369
Chelsea H. Komlo <chelsea.komlo@gmail.com>
2017-02-13 23:51:46 -0500

    start to implement api

--- chacha20.h
+++ chacha20.h
@@ -5,4 +5,11 @@
 #define BYTE_LENGTH 64
 #define NUMBER_ROUNDS 10
 
+void chacha20_encrypt(uint8_t *key,
+                 uint32_t counter,
+                 uint8_t *nonce,
+                 unsigned char *plaintext,
+                 uint32_t plaintext_length,
+                 unsigned char *ciphertext);
+
 #endif
